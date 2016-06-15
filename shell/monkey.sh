fe94d8e450aead54b35998235bd18ad70fd8dca6
snowdream <yanghui1986527@gmail.com>
2016-06-15 09:44:22 +0800

    Update: monkey.sh

--- shell/monkey.sh
+++ shell/monkey.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 PACKAGE_NAME="com.autonavi.minimap"
+DEBUG=true
 
 check_device()
 {
@@ -24,6 +25,21 @@ usage() {
 }
 
 start_monkey(){
+	if [[ DEBUG ]]; then
+		echo "\$@=$@"
+	fi
+
+	while getopts ":p" opt; do
+    case $opt in
+        p)shift
+				  PACKAGE_NAME=$1  
+					echo "PACKAGE_NAME=$1"
+				  ;;
+        \?) echo "Invalid param" ;;
+    esac
+ done
+	
+	
   adb shell "monkey -p ${PACKAGE_NAME} --ignore-crashes --ignore-timeouts --ignore-security-exceptions --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-anyevent 0 -v -v -v --throttle 500 1200000000 > /storage/sdcard0/monkey_log.txt  &"
 }
 
@@ -39,29 +55,19 @@ if [ $# == "0" ]; then
   exit
 fi
 
-while (( $# )); do
-case $1 in
---help|-h)
-  usage
-  exit
-  ;;
--p) 
-  shift; 
-  PACKAGE_NAME=$1 
-  ;;
-start)
-  start_monkey
-  ;;
-stop)
-  stop_monkey
-  exit
-  ;;
-*) # execute 'advanced command' by function name
-  adb shell monkey $@
-  ;;
-esac
-shift
-done  
+if [[ DEBUG ]]; then
+	echo "\$1=$1"
+fi
+
+if [[ $1 == "--help" ]] ||  [[ $1 == "-h" ]] ; then
+	usage
+elif [[ $1 == "start"  ]]; then
+	start_monkey $@
+elif [[ $1 == "stop"  ]]; then
+	stop_monkey
+else
+	usage
+fi
 }
 
 main $@
